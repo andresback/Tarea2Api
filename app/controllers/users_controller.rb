63fdf1f938de908ebc5149ca340c9dda22fcbb34
andresback <aback@uc.cl>
2017-03-31 15:27:35 -0300

    error fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :valid_update, only: [:update]
   rescue_from ActiveRecord::RecordNotFound, :with => :error_not_found
 
 
@@ -17,22 +18,31 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created
+    if params.has_key?(:id)
+      render json: {'error': 'no se puede crear usuario con id'}, status: 400
     else
-      render json: @user.errors, status: :unprocessable_entity
+      @user = User.new(user_params)
+
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
   end
 
   # PATCH/PUT /users/1
   def update
+  if user_params[:id]
+    render json: {'error': 'id no es modificable'}, status: 400
+  else
     if @user.update(user_params)
-      render json: @user
+      render json: @user, status: 200
     else
       render json: {'error': 'la modificacion ha fallado'}, status: 500
     end
+
+  end
   end
 
   # DELETE /users/1
@@ -46,12 +56,18 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
+    def valid_update
+      if user_params.count() > 1
+        render json: {'error': 'la modificacion ha fallado'}, status: 500
+      end
+    end
+
     def error_not_found
       render json: {'error': 'usuario no encontrado'}, status: 404
     end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:nombre, :apellido, :usuario, :twitter)
+      params.require(:user).permit(:id, :nombre, :apellido, :usuario, :twitter)
     end
 end
